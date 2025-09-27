8172c45640748b1d844c360398dc8cf29b4f144a
github-webtest <81388699+github-webtest@users.noreply.github.com>
2025-09-27 17:53:03 +0300

    Delete js directory

--- js/lang.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    const supportedLangs = ['en', 'de', 'fr', 'es', 'pt', 'tr', 'ru', 'zh'];
-    const browserLang = (navigator.language || navigator.userLanguage || 'en').substring(0, 2);
-
-    const userLang = supportedLangs.includes(browserLang) ? browserLang : 'en';
-
-    localStorage.setItem('language', userLang);
-
-})();
