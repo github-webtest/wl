90e43d5c4905420acc8384bef83f9efee7c56b9d
github-webtest <81388699+github-webtest@users.noreply.github.com>
2025-09-27 18:05:03 +0300

    index

---
+++ json/tasks_json.js
@@ -0,0 +1,45 @@
+/* Tasks */
+
+const Tasks_1_Title = {      
+  en: "Produce Farmer",
+  de: "Bauer produzieren",
+  fr: "Produire un fermier",
+  es: "Producir agricultor",
+  pt: "Produzir agricultor",
+  tr: "Çiftçi Üret",
+  ru: "Произвести фермера",
+  zh: "生产农民",
+};
+
+const Tasks_1_Desc = {      
+  en: "Farmers increase the kingdom's food production. Produce 1 farmer to increase food production.",  
+  de: "Bauern erhöhen die Nahrungsmittelproduktion des Königreichs. Produziere 1 Bauern, um die Nahrungsmittelproduktion zu steigern.",  
+  fr: "Les fermiers augmentent la production alimentaire du royaume. Produisez 1 fermier pour accroître la production alimentaire.",  
+  es: "Los agricultores aumentan la producción de alimentos del reino. Produce 1 agricultor para aumentar la producción de alimentos.",  
+  pt: "Os agricultores aumentam a produção de alimentos do reino. Produza 1 agricultor para aumentar a produção de alimentos.",  
+  tr: "Çiftçiler krallığın besin üretimini artırır. Besin üretimini artırmak için 1 çiftçi üret.",  
+  ru: "Фермеры увеличивают производство пищи в королевстве. Произведите 1 фермера, чтобы увеличить производство пищи.",  
+  zh: "农民能提高王国的食物产量。生产1名农民以增加食物产量。",  
+};
+
+const Tasks_2_Title = {      
+  en: "Train Spearman",
+  de: "Speerkämpfer ausbilden",
+  fr: "Former un lancier",
+  es: "Entrenar lancero",
+  pt: "Treinar lanceiro",
+  tr: "Spearman Üret",
+  ru: "Обучить копейщика",
+  zh: "训练长矛兵",
+};
+
+const Tasks_2_Desc = {      
+  en: "Soldiers allow you to defend the kingdom and fight other kingdoms. Train 1 spearman to strengthen your army.",  
+  de: "Soldaten ermöglichen es dir, das Königreich zu verteidigen und gegen andere Königreiche zu kämpfen. Bilde 1 Speerkämpfer aus, um deine Armee zu stärken.",  
+  fr: "Les soldats te permettent de défendre le royaume et de combattre d'autres royaumes. Formez 1 lancier pour renforcer votre armée.",  
+  es: "Los soldados te permiten defender el reino y luchar contra otros reinos. Entrena 1 lancero para fortalecer tu ejército.",  
+  pt: "Os soldados permitem que você defenda o reino e lute contra outros reinos. Treine 1 lanceiro para fortalecer seu exército.",  
+  tr: "Askerler krallığı savunmanı ve diğer krallıklarla savaşmanı sağlar. Ordunu güçlendirmek için 1 Spearman üret.",  
+  ru: "Солдаты позволяют защищать королевство и сражаться с другими королевствами. Обучите 1 копейщика, чтобы усилить свою армию.",  
+  zh: "士兵能让你保卫王国并与其他王国作战。训练1名长矛兵以增强你的军队。",  
+};
